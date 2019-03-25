1c9d6587fcb51024fde834bff16339016ef2c9b3
qqdipps <sdipp007@gmail.com>
2019-03-24 22:56:19 -0700

    fig out how to find length with out reassigning number

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -3,18 +3,12 @@
 # Space complexity: O(1)
 def is_palindrome(number)
   return false if !number || number < 0
-  length = length_of_num(number)
+  length = 1
+  until number / 10 ** length == 0
+    length += 1
+  end
   (length / 2).times do |i|
     return false if (number / (10 ** (length - i - 1))) % 10 != (number % (10 ** (i + 1))) / (10 ** i)
   end
   return true
-end
-
-def length_of_num(num)
-  length = 0
-  until num == 0
-    num /= 10
-    length += 1
-  end
-  return length
 end
