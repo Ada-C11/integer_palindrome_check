b98aa629d9c79e363bdfcb0c46112b20330444d2
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-03-27 12:06:05 -0700

    pass all tests

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,6 +1,20 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(log10(n)), where n is the size of the integer
+# Space complexity: O(1)
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil || number < 0
+
+  until number < 10
+    i = Math.log10(number).to_i
+
+    if number % 10 != number / 10 ** i
+      return false
+    end
+    number -= (number / 10 ** i) * 10 ** i
+    number /= 10
+    if number % 10 == 0
+      number /= 10
+    end
+  end
+  return true
 end
