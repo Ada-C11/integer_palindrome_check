2b5537bd92636ebefb2780c0d0b8125b25c18769
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-03-27 15:31:47 -0700

    replaced Math.log10 with repeated division

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -3,18 +3,23 @@
 # Space complexity: O(1)
 def is_palindrome(number)
   return false if number == nil || number < 0
-
-  until number < 10
-    i = Math.log10(number).to_i
+  i = 0
+  placeholder = number
+  until placeholder == 0
+    placeholder /= 10
+    i += 1
+  end
+  until i <= 0
+    i -= 1
 
     if number % 10 != number / 10 ** i
       return false
     end
+
     number -= (number / 10 ** i) * 10 ** i
     number /= 10
-    if number % 10 == 0
-      number /= 10
-    end
+
+    i -= 1
   end
   return true
 end
