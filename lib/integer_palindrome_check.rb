63795554765f314a7b6735bf7083c90154c02ecc
Carla Bosco <carlabosco@gmail.com>
2019-03-27 20:00:47 -0700

    Updated method with time and space complexities

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,6 +1,29 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: linear, where all digits in number will be checked
+# Space complexity: constant, where i, first, and last are updated at each iteration
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil
+  return true if number >= 0 && number < 10 
+
+  # Find out how many digits number has
+  i = 1
+  while (number/i) >= 10
+    i *= 10
+  end
+
+  # Get first and last digits for comparison
+  while number != 0
+    first = number/i
+    last = number % 10
+
+    if first != last
+      return false
+    end
+    
+    # Drop first and last digits and update i
+    number = (number % i)/10
+    i = i/100
+  end
+  return true
 end
+
