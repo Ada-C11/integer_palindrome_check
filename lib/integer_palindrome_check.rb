2994f1a8725b81689a755ae1e96dedeb41be29aa
Amy Wyatt <amy.wyatt12@outlook.com>
2019-03-29 18:21:36 -0700

    all but two tests pass

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -2,5 +2,22 @@
 # Time complexity: ?
 # Space complexity: ?
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil
+
+  return true if number >= 0 && number <= 9
+
+  high = 10
+  low = 10
+
+  while number / high > 10
+    high *= 10
+  end
+
+  while high > 0
+    return false if number % high != number / low
+    high /= 10
+    low *= 10
+  end
+
+  return true
 end
