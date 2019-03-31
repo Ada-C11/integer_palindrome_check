a8927ae6b1bd635dafed56a3e011849a252bf8f8
Grace Shea <grace.m.shea@gmail.com>
2019-03-30 18:37:47 -0700

    passes all tests

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -2,25 +2,22 @@
 # Time complexity: ?
 # Space complexity: ?
 def is_palindrome(number)
-  return false if number >= 0 || number == nil
-  # in case of single digit4
-  return true if number >= 0 && number < 10
-  while number != 0
+  if number && number >= 0
+    while number != 0
+      # find number of digits
+      digit_count = find_digits(number)
+      first = number / 10 ** digit_count
+      last = number % 10
 
-    # find number of digits
-    digit_count = find_digits(number)
-    first = number / 10 ** digit_count
-    last = number % 10
-
-    if first != last
-      return false
+      return false if first != last
+      # Drop first and last digits
+      number = number % 10 ** digit_count
+      number = number / 10
     end
-
-    # Drop first and last digits
-    number = number % digit_count / 10
-    number = number / 10
+    return true
+  else
+    return false
   end
-  return true
 end
 
 def find_digits(number)
