d183deaf96c280b3c0f42812e3860a15d0e8b781
Stephanie Marchante <smarcha04@gmail.com>
2019-03-31 14:21:44 -0700

    implemented d integer palindrome check

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,6 +1,23 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the amount of digits in the number
+# Space complexity: O(1)
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil || number < 0
+  return true if number == 0 || number < 10
+
+  exponent = 1
+  while (number / 10 ** exponent) > 9 do
+    exponent += 1
+  end
+
+  until number == 0
+    if (number / 10 ** exponent) == (number % 10)
+      number = (number % (10 ** exponent)) / 10
+      exponent -= 2 
+    else
+      return false
+    end
+  end
+
+  return true
 end
