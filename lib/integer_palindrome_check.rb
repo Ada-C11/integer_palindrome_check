dbdfbccc9019d4b33808fd9f789c64a9f6fa38e7
Grace Shea <grace.m.shea@gmail.com>
2019-03-30 17:34:03 -0700

    inital method mapped out, doesn't work yet

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -2,5 +2,31 @@
 # Time complexity: ?
 # Space complexity: ?
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number >= 0 || number == nil
+  # in case of single digit
+  return true if number >= 0 && number < 10
+  while number != 0
+
+    # find number of digits
+    digit_count = find_digits(number)
+    first = number / 10 ** digit_count
+    last = number % 10
+
+    if first != last
+      return false
+    end
+
+    # Drop first and last digits
+    number = number % digit_count / 10
+    number = number / 10
+  end
+  return true
+end
+
+def find_digits(number)
+  digits = 1
+  while (number / digits) >= 10
+    digits *= 10
+  end
+  return digits
 end
