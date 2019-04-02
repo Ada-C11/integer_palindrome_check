977bbdd718b7f1d4a2e014da5c49a3d5b9085673
amyesh <amyesh08@gmail.com>
2019-04-02 13:38:43 -0700

    completed int pal check

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,6 +1,26 @@
-# Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Returns true if the input positive integer number
+# forms a palindrome. Returns false otherwise.
+# Time complexity: O(n) where n equals the length of the
+# integer.
+# Space complexity: O(1) the space complexity is constant
+# because no additional space is required, and the program
+# just returns either true or false.
 def is_palindrome(number)
-  raise NotImplementedError
+  if number == nil
+    return false
+  end
+  div = 1
+  while (number / div) >= 10
+    div *= 10
+  end
+  while number != 0
+    first = number / div
+    last = number % 10
+    if first != last
+      return false
+    end
+    number = (number % div) / 10
+    div = div / 100
+  end
+  return true
 end
