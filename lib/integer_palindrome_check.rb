c29e489fe7fd3877af4f8b39eda4f4604ddcfb99
Erica Norris <norrise120@gmail.com>
2019-04-03 13:46:52 -0700

    Cteated is_palindrome method to pass tests

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -2,5 +2,19 @@
 # Time complexity: ?
 # Space complexity: ?
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil || number < 0
+  i = 10
+  while (number % (i)) != number
+    i *= 10
+  end
+  i /= 10
+  while i > 10
+    if (number / i) == (number % 10)
+      number = (number % i) / 10
+      i /= 100
+    else
+      return false
+    end
+  end
+  return true
 end
