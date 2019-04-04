14deb5f7bde46f0e10f1de75bc6011c68b862bd6
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-03 18:17:53 -0700

    Changed space/time complexity

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,8 +1,8 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
-# Time complexity: O(n^2), where n is the number of digits in the number and there are 2 loops.
-# Space complexity: O(1)
-def is_palindrome(number)
-  reversed_number = []
+# Time complexity: O(n), where n is the number of digits in the number
+# Space complexity: O(n) number of digits
+def is_palindrome(number) # 12343567
+  reversed_number = [] # 7653421
 
   if (number == nil) || (number < 0)
     # puts "APPLE"
