b26808bc2e2618413fd33ffa97a49284de828933
Jansen <v.jansen.martin@gmail.com>
2019-04-03 18:51:18 -0700

    All tests passing; Add time and space complexity explanations

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,6 +1,20 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: Linear or O(n), where n is the number of characters inside the string.
+# Space complexity: Constant or O(1), because additional space doesn't
+# depend on array size or input
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil || number < 0
+  return true if number < 10
+
+  n = number
+  reverse = 0
+
+  until n <= 0
+    remainder = n % 10
+    reverse = (reverse * 10) + remainder
+
+    n /= 10
+  end
+
+  return reverse == number
 end
